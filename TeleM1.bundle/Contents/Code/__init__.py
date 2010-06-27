568b01b72bc74ca5c5ec0a0414593b98771bb0bc
Christian Hitz <christian@klarinett.li>
2010-06-27 15:52:31 +0200

    Implement video display.

--- TeleM1.bundle/Contents/Code/__init__.py
+++ TeleM1.bundle/Contents/Code/__init__.py
@@ -3,6 +3,20 @@ from PMS import *
 from PMS.Objects import *
 from PMS.Shortcuts import *
 
+from textwrap import dedent
+
+def gettext(elem):
+    if elem.text:
+        text = dedent(elem.text)
+    else:
+        text = ""
+    for subelem in elem:
+        text = text + gettext(subelem)
+        if subelem.tail:
+            text = text + dedent(subelem.tail)
+
+    return '\n'.join(text.splitlines())
+
 ####################################################################################################
 
 VIDEO_PREFIX = "/video/telem1"
@@ -28,7 +42,7 @@ def Start():
     ## see also:
     ##  http://dev.plexapp.com/docs/mod_Plugin.html
     ##  http://dev.plexapp.com/docs/Bundle.html#the-strings-directory
-    Plugin.AddPrefixHandler(VIDEO_PREFIX, VideoMainMenu, L('VideoTitle'), ICON, ART)
+    Plugin.AddPrefixHandler(VIDEO_PREFIX, VideoMainMenu, L('Title'), ICON, ART)
 
     Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
     Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
@@ -42,36 +56,15 @@ def Start():
     MediaContainer.title1 = NAME
     DirectoryItem.thumb = R(ICON)
 
-  
-
-
-#### the rest of these are user created functions and
-#### are not reserved by the plugin framework.
-#### see: http://dev.plexapp.com/docs/Functions.html for
-#### a list of reserved functions above
-
-
-
-#
-# Example main menu referenced in the Start() method
-# for the 'Video' prefix handler
-#
 
 def VideoMainMenu():
-
-    # Container acting sort of like a folder on
-    # a file system containing other things like
-    # "sub-folders", videos, music, etc
-    # see:
-    #  http://dev.plexapp.com/docs/Objects.html#MediaContainer
     dir = MediaContainer(viewGroup="InfoList")
 
     xml = XML.ElementFromURL(SHOW_LIST_URL, True)
     for show in xml.xpath("//div[@id='sidebar']//li[not(contains(@class, 'first'))]"):
         title = show.xpath("a")[0].text
-        url = show.xpath("a")[0].get('href')
-        Log(title + ": " + url)
-        show_page = XML.ElementFromURL(MAIN_URL + url, True)
+        show_url = show.xpath("a")[0].get('href')
+        show_page = XML.ElementFromURL(MAIN_URL + show_url, True)
         try:
             description = show_page.xpath("//div[@class='content']/p")[0].text
         except:
@@ -87,7 +80,8 @@ def VideoMainMenu():
                     title,
                     summary=description,
                     thumb=thumb,
-                )
+                ),
+                url = show_url,
             )
         )
 
@@ -95,9 +89,74 @@ def VideoMainMenu():
     # ... and then return the container
     return dir
 
-def ShowDetails(sender):
+
+def RetreiveVideoURL(thread_url, title=None, description=None, thumb=None):
+        thread_page = HTTP.Request(thread_url)
+        thread_lines = thread_page.splitlines()
+        url = None
+        for line in thread_lines:
+            if (line.find("var movieNameId = ") >= 0):
+                url = MAIN_URL + line.split('"')[1]
+
+        return VideoItem(
+                    url,
+                    title,
+                    summary = description,
+                    thumb = thumb,
+                )
+
+
+def ShowDetails(sender, url):
     dir = MediaContainer(viewGroup="InfoList")
+
+    show_page = XML.ElementFromURL(MAIN_URL + url, True)
+    show_number = -1
+    for show in show_page.xpath("//div[@class='showContainer']"):
+        show_number += 1
+
+        try:
+            thumb = MAIN_URL + show.xpath("div[@class='showOverviewImageContainer']/img")[0].get('src')
+        except:
+            thumb = None
+
+        title = show.xpath("div[@class='showOverviewContentContainer']/h2")[0].text
+
+        try:
+            description = gettext(show.xpath("div/div/div[@class='showOverviewInformationContainer']")[0])
+        except:
+            description = None
+
+        threads = show.xpath("div/div/div/ul[@class='showThreads']/li")
+
+        if (len(threads) == 1):
+            thread_url = MAIN_URL + threads[0].xpath("a")[0].get('href')
+            dir.Append(RetreiveVideoURL(thread_url, title, description, thumb))
+        else:
+            dir.Append(
+                Function(
+                    DirectoryItem(
+                        ShowParts,
+                        title,
+                        summary=description,
+                        thumb=thumb,
+                    ),
+                    url = url,
+                    show_number = show_number
+                )
+            )
+    return dir
+
+
+def ShowParts(sender, url, show_number):
+    dir = MediaContainer(viewGroup="InfoList")
+
+    show_page = XML.ElementFromURL(MAIN_URL + url, True)
+    show = show_page.xpath("//div[@class='showContainer']")[show_number]
+
+    for thread in show.xpath("div/div/div/ul[@class='showThreads']/li"):
+        title = thread.xpath("a")[0].text
+        thread_url = MAIN_URL + thread.xpath("a")[0].get('href')
+        dir.Append(RetreiveVideoURL(thread_url, title))
 
     return dir
 
-  
